4ab6e094bbabed800ae985d7d1b246f586506fe6
Paige Cote <pmcote1@gmail.com>
2016-04-11 10:52:31 -0400

    added foundation with flex-grid and gitignore

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,37 @@ var routes = {};
 var util = require('util');
 
 routes.home = function(req, res) {
-	res.render('home');
-}
+	res.render('signIn', {'students':
+		[
+      {
+        'name': 'Paige'
+      },
+      {
+        'name' : 'Avalon'
+      },
+			{
+        'name': 'Paige'
+      },
+			{
+        'name': 'Paige'
+      },
+			{
+        'name': 'Paige'
+      },
+			{
+        'name': 'Paige'
+      },
+			{
+        'name': 'Paige'
+      },
+			{
+        'name': 'Paige'
+      },
+			{
+        'name': 'Paige'
+      },
+		]
+	});
+};
 
 module.exports = routes;
